4bfbad9f48db0999b00f4d948da2ed1abe0fe56c
accrazed <accrazed@gmail.com>
2023-01-29 20:53:04 -0500

    10: implement login endpoint

--- src/routes/auth.go
+++ src/routes/auth.go
@@ -8,11 +8,68 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/polyamanita/polyamanita-server/src/lib"
 )
 
-func (c *Controller) Login(ctx *gin.Context) { ctx.Status(http.StatusNotImplemented) }
+func (c *Controller) Login(ctx *gin.Context) {
+	// Input for registeration
+	type LoginInputStruct struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	body := &LoginInputStruct{}
+	if err := ctx.BindJSON(body); err != nil {
+		c.l.Error(err)
+		ctx.Status(http.StatusBadRequest)
+		return
+	}
+
+	expr, err := expression.NewBuilder().
+		WithFilter(expression.Name("email").Equal(expression.Value(body.Email)).
+			And(expression.Name("password").Equal(expression.Value(body.Password)))).
+		Build()
+	if err != nil {
+		c.l.Error(err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+	scanResp, err := c.DynamoDB.Scan(&dynamodb.ScanInput{
+		TableName:                 aws.String(c.secrets.ddbUserbaseTable),
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		FilterExpression:          expr.Filter(),
+	})
+	if err != nil {
+		c.l.Error(err)
+		ctx.Status(http.StatusUnauthorized)
+		return
+	}
+	if *scanResp.Count == 0 {
+		c.l.Debug(fmt.Sprintf(`invalid credentials for %v`, body.Email))
+		ctx.Status(http.StatusBadRequest)
+		return
+	}
+
+	id := *scanResp.Items[0]["id"].S
+	token, err := lib.NewSignedToken(id, c.secrets.jwtKey, 24*time.Hour)
+	if err != nil {
+		c.l.Error(err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+
+	type LoginOutputStruct struct {
+		Id          string `json:"id"`
+		AccessToken string `json:"accessToken"`
+	}
+	ctx.JSON(http.StatusOK, LoginOutputStruct{
+		Id:          id,
+		AccessToken: token,
+	})
+}
 
 func (c *Controller) Logout(ctx *gin.Context) { ctx.Status(http.StatusNotImplemented) }
 
