9851cf72bdaabfa281d3c411f8b9dfd80b05ecb1
accrazed <accrazed@gmail.com>
2023-02-11 12:11:25 -0500

    add getCapture endpoint

--- src/routes/captures.go
+++ src/routes/captures.go
@@ -1,17 +1,74 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/gin-gonic/gin"
+	"github.com/polyamanita/polyamanita-server/src/models"
 )
 
 func (c *Controller) GetCapturesList(ctx *gin.Context) { ctx.Status(http.StatusNotImplemented) }
 
-func (c *Controller) AddCaptures(ctx *gin.Context) { ctx.Status(http.StatusNotImplemented) }
+// AddCaptures godoc
+//	@Summary		Add a new list of captures to the user
+//	@Description	Gets a list of mushrooms to add as captures to the user journal
+//	@Tags			Auth
+//	@Accept			json
+//	@Produce		json
+//	@Param			request	body		routes.AddCaptures.AddCapturesInputStruct	true	"Email address to send code to"
+//	@success		200		{object}	routes.AddCaptures.AddCapturesOutputStruct	"Expiry time of code"
+//	@Failure		500
+//	@Router			/users/:UserID/captures [post]
+func (c *Controller) AddCaptures(ctx *gin.Context) {
+	type AddCapturesInputStruct struct{}
+
+	type AddCapturesOutputStruct struct{}
+}
 
 func (c *Controller) DeleteCaptures(ctx *gin.Context) { ctx.Status(http.StatusNotImplemented) }
 
-func (c *Controller) GetCapture(ctx *gin.Context) { ctx.Status(http.StatusNotImplemented) }
+// GetCapture godoc
+//	@Summary		Get information about a captured mushroom
+//	@Description	Gets all relevant information about a mushroom that's been captured for a user
+//	@Tags			Auth
+//	@Accept			json
+//	@Produce		json
+//	@success		200		{object}	routes.GetCapture.GetCaptureOutputStruct	"mushroom information"
+//	@Failure		500
+//	@Router			/users/:UserID/captures/:CaptureID [get]
+func (c *Controller) GetCapture(ctx *gin.Context) {
+	UserID := ctx.Param("UserID")
+	CaptureID := ctx.Param("CaptureID")
+
+	res, err := c.DynamoDB.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(c.secrets.ddbUserbaseTable),
+		Key: map[string]*dynamodb.AttributeValue{
+			"UserID":   {S: aws.String(UserID)},
+			"MainSort": {S: aws.String(fmt.Sprintf("Capture#%s#%s", UserID, CaptureID))},
+		},
+	})
+	if err != nil {
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+	if len(res.Item) == 0 {
+		ctx.Status(http.StatusNotFound)
+	}
+
+	type GetCaptureOutputStruct struct {
+		Capture *models.Capture `json:"capture"`
+	}
+	response := GetCaptureOutputStruct{}
+	if err := dynamodbattribute.UnmarshalMap(res.Item, &response.Capture); err != nil {
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, response)
+}
 
 func (c *Controller) DownloadCaptureImage(ctx *gin.Context) { ctx.Status(http.StatusNotImplemented) }
---
+++ src/routes/captures_test.go
@@ -0,0 +1,81 @@
+package routes_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/gin-gonic/gin"
+	"github.com/polyamanita/polyamanita-server/src/fakes"
+	"github.com/polyamanita/polyamanita-server/src/lib"
+	"github.com/polyamanita/polyamanita-server/src/models"
+	"github.com/polyamanita/polyamanita-server/src/routes"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetCapture(t *testing.T) {
+	wantCapture := &models.Capture{
+		CaptureID:  "some-capture-id",
+		UserID:     "some-user-id",
+		TimesFound: 10,
+		Notes:      "some notes",
+		Instances: []*models.Instance{
+			{
+				Longitude: 101,
+				Latitude:  102,
+				Location:  "Forest",
+				DateFound: time.Date(2022, 2, 22, 22, 22, 22, 22, time.UTC),
+			},
+		},
+	}
+	GetItemOutput, err := dynamodbattribute.Marshal(wantCapture)
+	assert.NoError(t, err)
+	dynamoMock := &fakes.DynamoDBAPI{}
+	dynamoMock.GetItemCall.Returns.GetItemOutput = &dynamodb.GetItemOutput{
+		Item: GetItemOutput.M,
+	}
+
+	cm := routes.NewTestController(nil, dynamoMock, nil, lib.NewLogger(os.Stdout))
+
+	w := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(w)
+	ctx.Request = httptest.NewRequest(
+		http.MethodGet,
+		"/users/some-user-id/captures/some-capture-id",
+		nil,
+	)
+	ctx.Params = []gin.Param{
+		{Key: "UserID", Value: "some-user-id"},
+		{Key: "CaptureID", Value: "some-capture-id"},
+	}
+
+	// Make call
+	cm.GetCapture(ctx)
+
+	// Check GetItem call
+	assert.Equal(t,
+		map[string]*dynamodb.AttributeValue{
+			"UserID":   {S: aws.String("some-user-id")},
+			"MainSort": {S: aws.String(fmt.Sprintf("Capture#%s#%s", "some-user-id", "some-capture-id"))},
+		},
+		dynamoMock.GetItemCall.Receives.GetItemInput.Key,
+	)
+
+	assert.Equal(t, aws.String("some-userbase-table"), dynamoMock.GetItemCall.Receives.GetItemInput.TableName)
+
+	// Check body response
+	gotBody := &struct {
+		Capture *models.Capture `json:"capture"`
+	}{}
+	json.NewDecoder(w.Body).Decode(gotBody)
+
+	assert.Equal(t, wantCapture, gotBody.Capture)
+	assert.Equal(t, http.StatusOK, ctx.Writer.Status())
+}
